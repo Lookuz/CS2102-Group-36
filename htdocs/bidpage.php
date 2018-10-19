693356bff247f86718d3c999599b4a39b3b0093e
Lookaz <cweyyeh512@gmail.com>
2018-10-19 23:34:06 +0800

    Made more awesome changes to bidpage.php

--- htdocs/bidpage.php
+++ htdocs/bidpage.php
@@ -14,6 +14,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     </head>
     
     <body>
@@ -50,7 +51,7 @@
                         </div>
                     </div>
                     <div class="form-container col-sm-8 offset-sm-2 rounded ">
-                        <form>
+                        <form method="post">
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-group w-100">
@@ -58,7 +59,8 @@
                                         <p class="form-control"><?php echo $_GET['date']; ?></p>
                                     </div>
                                     <div class="form-group w-100">
-                                        <label class="w-100">
+                                        <label class="w-100">Your Bid</label>
+                                        <p class="form-control">
                                         <?php
                                             $db = pg_connect("host=localhost port=5432 dbname=CarPool user=postgres password=2102");
 
@@ -72,8 +74,7 @@
                                                 echo '0';
                                             }
                                         ?>
-                                        </label>
-                                        <p class="form-control">0</p>
+                                        </p>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
@@ -82,7 +83,8 @@
                                         <p class="form-control"><?php echo $_GET['time']; ?></p>
                                     </div>
                                     <div class="form-group w-100">
-                                        <label class="w-100">
+                                        <label class="w-100">Highest Bid</label>
+                                        <p class="form-control">
                                         <?php
                                             $db = pg_connect("host=localhost port=5432 dbname=CarPool user=postgres password=2102");
 
@@ -96,15 +98,14 @@
                                                 echo '0';
                                             }
                                         ?>
-                                        </label>
-                                        <p class="form-control">0</p>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 offset-sm-3">
                                 <div class="form-group">
                                     <label class="w-100">Enter New Bid</label>
-                                    <input class="form-control w-100" type="number" min="1">
+                                    <input class="form-control w-100" type="number" min="1" name="new-bid">
                                 </div>
                             </div>
                             <div class="col-sm-4 offset-sm-4">
@@ -118,83 +119,6 @@
     </body>
 </html>
 
-<!-- <html>
-    <head>
-        <title>
-            Carpool
-        </title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bidpage.css">
-    </head>
-    
-    <body>
-        <?php
-            include 'partials/navbar.php';
-        ?>
-        <div class="h-100">
-            <h1 class='display-2'> Bid Page </h1>
-            <div class='main-bid-box'>
-                <form class="" method="POST">
-                    <div class="row"> 
-                        <div class="col-sm-3">
-                        </div>
-                        <div class="col-sm">
-                            <div class="row field-item"><?php echo $_GET['origin']; ?></div>
-                            <div class="row field-item"><?php echo $_GET['destination']; ?></div>
-                        </div>
-                    </div>
-                    <div class="row"> 
-                        <div class="col-sm field-item">
-                            <?php echo $_GET['date']; ?>
-                        </div>
-                        <div class="col-sm field-item"><?php echo $_GET['time']; ?></div>
-                    </div>
-                    <div class="row"> 
-                        <div class="col-sm field-item">
-                            <?php
-                                $db = pg_connect("host=localhost port=5432 dbname=CarPool user=postgres password=2102");
-
-                                $sql = 'SELECT get_user_bid_func($1, $2, $3, $4, $5)';
-                                $res = pg_prepare($db, 'get_user_bid', $sql);
-                                $res = pg_execute($db, 'get_user_bid', 
-                                    array($_SESSION['email'], $_GET['origin'], $_GET['destination'], $_GET['date'], $_GET['time']));
-                                if($bid = pg_fetch_result($res, 0)) {
-                                    echo $bid;
-                                } else {
-                                    echo '0';
-                                }
-                            ?>
-                        </div>
-                        <div class="col-sm field-item">
-                            <?php
-                                $db = pg_connect("host=localhost port=5432 dbname=CarPool user=postgres password=2102");
-
-                                $sql = 'SELECT get_max_bid_func($1, $2, $3, $4)';
-                                $res = pg_prepare($db, 'get_max_bid', $sql);
-                                $res = pg_execute($db, 'get_max_bid', 
-                                    array($_GET['origin'], $_GET['destination'], $_GET['date'], $_GET['time']));
-                                if($bid = pg_fetch_result($res, 0)) {
-                                    echo $bid;
-                                } else {
-                                    echo '0';
-                                }
-                            ?>
-                        </div>
-                    </div>
-                    
-                    <div class="row justify-content-center"> 
-                        <input class="col-9 field-item" name="new-bid" type="number" min="1">
-                    </div>  
-                    
-                    <div class="row justify-content-center"> 
-                        <button class="col-3 field-item btn btn-outline-info btn-rounded btn-block waves-effect" type="submit" name="Bid">GO!</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </body>
-</html> -->
-
 <?php
     if (isset($_POST['Bid'])) {
         $db = pg_connect("host=localhost port=5432 dbname=CarPool user=postgres password=2102");
@@ -204,6 +128,6 @@
         $res = pg_execute($db, 'user_bid', array($_GET['origin'], $_GET['destination'], $_GET['date'], $_GET['time'], 
             $_SESSION['email'], $_POST['new-bid']));
 
-        header("Refresh:0; index.php");
+        header("Refresh:0; url=index.php");
     }
 ?>
