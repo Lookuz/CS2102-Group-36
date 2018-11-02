c6d39bbd8b7d181f0d9e096144a88b01846af5e8
Lookaz <cweyyeh512@gmail.com>
2018-11-03 00:28:29 +0800

    Updated create_ride.sql

--- sql/create_ride.sql
+++ sql/create_ride.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE FUNCTION create_ride
-(c_plate TEXT, r_date_str TEXT, r_time_str TEXT, r_origin TEXT, r_destination TEXT)
+(c_plate_arg TEXT, r_date_str TEXT, r_time_str TEXT, r_origin_arg TEXT, r_destination_arg TEXT)
 RETURNS void
 AS
 $$
 DECLARE
-	r_date DATE;
-	r_time TIME;
+	r_date_arg DATE;
+	r_time_arg TIME;
 BEGIN
-	r_date = string_to_date(r_date_str);
-	r_time = string_to_time(r_time_str);
+	r_date_arg = string_to_date(r_date_str);
+	r_time_arg = string_to_time(r_time_str);
 
-	INSERT INTO rides
-	VALUES(c_plate, r_date, r_time, r_origin, r_destination);
+	INSERT INTO rides(c_plate, r_date, r_time, r_origin, r_destination)
+	VALUES(c_plate_arg, r_date_arg, r_time_arg, r_origin_arg, r_destination_arg);
 END
 $$
 LANGUAGE plpgsql;
